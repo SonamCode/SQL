5102db78fe6472dce00be0fc4419d41bd0813571
SonamCode <sona22122708@gmail.com>
2025-08-29 13:56:20 +0530

    sql updated

---
+++ create_database.sql
@@ -0,0 +1,7 @@
+INSERT INTO customer
+("ID", "Name", "Age", "City", "Salary")
+VALUES
+(1, 'Sonam', 26, 'Bihar Saharif', 25000),
+(2, 'Nishi', 27, 'Nalanda', 27000),
+(3, 'Disha', 28, 'Ranchi', 30000),
+(4, 'Juhi', 29, 'Bihar Sharif', 38000);
